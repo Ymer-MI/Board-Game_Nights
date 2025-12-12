c093221dd824d67e851b2afb35ad3e138a802010
ymer94 <ymer_94@hotmail.com>
2025-12-12 17:59:29 +0100

    Updated to use helper serverFunctions instead.
    Changes to be committed:
    	modified:   src/app/page.tsx
    	modified:   src/helpers/serverFunctions.ts

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,21 +1,21 @@
 import { getBGGDB } from '@/lib/getBGGDB'
-import BoardGameNightsAPI from '@/services/boardGameNightsAPI'
 import Event from '@/models/Event'
 import { LogButton } from '@/components/dev/LogButton'
 import ServerStatus from '@/components/dev/ServerStatus'
 import GamesList from '@/components/dev/GamesList'
 import EventList from '../components/EventList/EventList'
 import CreateClientForm from '@/components/CreateClientForm/CreateClientForm'
+import { getEvents } from '@/helpers/serverFunctions'
 
 export default async function Home() {
-    const DB = { BGG: await getBGGDB(), Strapi: new BoardGameNightsAPI() }, events = (await DB.Strapi.getEvents()).data?.map(e => new Event(e)) ?? []
+    const BGGDB = await getBGGDB(), events = (await getEvents()).data?.map(e => new Event(e)) ?? []
 
     return <>
         <h1>Board Game Nights</h1>
         {/* <ServerStatus status={ await (async () => { const r = await DB.Strapi.ping(); r.status.toString().match(/^-1$/) && console.error('Error pinging server:', r.statusText); return r.status.toString().startsWith('2') && r.statusText.match(/^OK$/) ? 'Online' : 'Offline' })() }/> */}
-        {/* <GamesList BGGDB={ DB.BGG } itemsToDisplay={ 5 }/> */}
-        {/* <EventList events={ events } BGGDB={ DB.BGG }/> */}
-        {/* <LogButton str={JSON.stringify(DB.BGG.getDB())}/> */}
+        {/* <GamesList BGGDB={ BGGDB } itemsToDisplay={ 5 }/> */}
+        {/* <EventList events={ events } BGGDB={ BGGDB }/> */}
+        {/* <LogButton str={JSON.stringify(BGGDB.getDB())}/> */}
         <CreateClientForm/>
     </>
 }
--- front-end/src/helpers/serverFunctions.ts
+++ front-end/src/helpers/serverFunctions.ts
@@ -9,6 +9,10 @@ export async function getToken(str: string) {
     return await JWT.getToken(str)
 }
 
+export async function getEvents() {
+    return await API.getEvents()
+}
+
 export async function createClient(data: any) {
     return await API.createClient(data)
 }
