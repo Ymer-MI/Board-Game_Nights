c0546f88bcb83b06ce218a2bdb0411f24527ef9b
ymer94 <ymer_94@hotmail.com>
2025-12-22 09:25:34 +0100

    Updated: Changed to a detailss button component.
    Changes to be committed:
    	new file:   front-end/src/components/EventList/DetailsButton.tsx
    	modified:   front-end/src/components/EventList/EventItem.tsx
    	new file:   front-end/src/components/EventList/detailsButton.module.css
    	modified:   front-end/src/components/EventList/eventList.module.css
    	modified:   front-end/src/styles/globals.css

--- front-end/src/components/EventList/EventItem.tsx
+++ front-end/src/components/EventList/EventItem.tsx
@@ -1,13 +1,15 @@
 import styles from './eventList.module.css'
+import buttonStyles from './detailsButton.module.css'
 import { IEvent } from '@/models/Event'
 import { useState } from 'react'
 import { Icon } from '@iconify/react'
 import { IBGGDBRow } from '@/models/BGGDB'
+import DetailsButton from './DetailsButton'
 
 export default function EventItem({ event, gameName }: { event: IEvent, gameName: IBGGDBRow['name'] }) {
-    const [isDetailsOpened, setIsDetailsOpened] = useState(false)
+    const [isDetailsOpened, setIsDetailsOpened] = useState(false), toggleDetails = () => { setIsDetailsOpened(!isDetailsOpened) }
 
-    return <li id={ event.documentId } className={ styles.event } onClick={() => { setIsDetailsOpened(!isDetailsOpened) }}>
+    return <li id={ event.documentId } className={ styles.event }>
         <h4>{ gameName } @{ event.location }</h4>
         <div className={ styles.infoRow }>
             <div>
@@ -15,19 +17,19 @@ export default function EventItem({ event, gameName }: { event: IEvent, gameName
                 <span className={ styles.bannerDetail }>Players: { event.playersMin } - { event.playersMax }</span>
                 <span className={ styles.bannerDetail }>Host: { event.host?.name }</span>
             </div>
-            <span className={ `${ styles.detailsClosed } ${ isDetailsOpened && styles.hidden }` }>Details <Icon icon='system-uicons:chevron-down' height={ 30 } width={ 20 }></Icon></span>
+            <div>
+                <DetailsButton className={ isDetailsOpened && buttonStyles.hidden } icon='system-uicons:pull-down' onClick={ toggleDetails }/>
+            </div>
         </div>
         <section className={ `${ styles.description } ${ !isDetailsOpened && styles.hidden }` }>
             <p>
                 { event.description }
             </p>
             <div className={ styles.attendeeRow }>
-                <span>
-                    Attending: { event.attendees?.map((c, i, a) => <>
-                        <span key={ c.documentId }>{ c.name }</span>{ i !== --a.length && ','}
-                    </>) }
-                </span>
-                <span className={ `${ styles.detailsOpened } ${ !isDetailsOpened && styles.hidden }` }>Details <Icon icon='system-uicons:chevron-up' height={ 30 } width={ 20 }></Icon></span>
+                <span>Attending: { event.attendees?.map((c, i, a) => <><span key={ c.documentId }>{ c.name }</span>{ i !== --a.length && ','}</>) }</span>
+                <div>
+                    <DetailsButton className={ !isDetailsOpened && buttonStyles.hidden } icon='system-uicons:push-up' onClick={ toggleDetails }/>
+                </div>
                 <div className={ styles.attendContainer }>
                     <button onClick={() => { console.log('Attending') }}>Attend<Icon icon='system-uicons:write'></Icon></button>
                 </div>
